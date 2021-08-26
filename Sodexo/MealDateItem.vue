cc0eeafe4622f4b3e030f4351503d5f9c2c3c45a
Aale Lehtoniemi <aalehton@edu.lapinamk.fi>
2021-08-26 14:59:56 +0300

    Nested Git Repository away

---
+++ Sodexo/MealDateItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <view class="container">
+    <text class="date">{{item.date}}</text>
+    <meal-item
+      class="text-container"
+      v-for="meal in meals"
+      :key="meal.title_fi"
+      :item="meal"
+    />
+  </view>
+</template>
+
+<script>
+import MealItem from "./MealItem"
+export default {
+  props: {
+    item: {
+      Type: Object
+    }
+  }
+};
+</script>
+
+<style>
+  .container {
+    margin: 10;
+  }
+  .date {
+    color:coral;
+    font-size: 20px;
+  }
+</style>
